c12496fbd0a9b2382a63dd4ce0342113a53321c9
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2015-03-08 15:28:31 +0900

    fix evaluator's order

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -9,15 +9,21 @@ import           Language.Haskell.Interpreter
 type Script    = String -> IO String
 type Evaluator = String -> Interpreter Script
 
-types :: [(String, String, String, String)]
-types =
-  [ ("String", "string", "id", "id")
-  , ("[String]", "lines", "lines", "unlines")
-  , ("[[String]]", "table", "map words . lines", "unlines . map unwords")
-  , ("a", "value", "read", "(++ \"\\n\") . show")
-  , ("[a]", "value lines", "map read . lines", "unlines . map show")
-  , ("[[a]]", "value table", "map (map read . words) . lines", "unlines . map (unwords . map show)")
-  ]
+types :: Bool -> [(String, String, String, String)]
+types isInput
+  | isInput = ss ++ vs
+  | otherwise = reverse ss ++ reverse vs
+  where
+    ss =
+      [ ("String", "string", "id", "id")
+      , ("[String]", "lines", "lines", "unlines")
+      , ("[[String]]", "table", "map words . lines", "unlines . map unwords")
+      ]
+    vs =
+      [ ("a", "value", "read", "(++ \"\\n\") . show")
+      , ("[a]", "value lines", "map read . lines", "unlines . map show")
+      , ("[[a]]", "value table", "map (map read . words) . lines", "unlines . map (unwords . map show)")
+      ]
 
 evals :: [(String, String, Evaluator)]
 evals =
@@ -28,18 +34,18 @@ evals =
   ] ++
   [ evaluator outputType ("output " ++ outputDescr)
       (\expr -> "\\_ -> return . " ++ outputCode ++ " . " ++ expr)
-  | (outputType, outputDescr, _, outputCode) <- reverse types
+  | (outputType, outputDescr, _, outputCode) <- types False
   ] ++
   [ evaluator ("IO " ++ outputType) ("output result" ++ outputDescr)
       (\expr -> "\\_ -> return . " ++ outputCode ++ " =<< " ++ expr)
-  | (outputType, outputDescr, _, outputCode) <- reverse types
+  | (outputType, outputDescr, _, outputCode) <- types False
   ] ++
   [ evaluator
       (inputType ++ " -> " ++ outputType)
       ("transform " ++ inputDescr ++ " to " ++ outputDescr)
       (\expr -> "return . " ++ outputCode ++ " . " ++ expr ++ " . " ++ inputCode)
-  | (inputType,  inputDescr,  inputCode, _)  <- types
-  , (outputType, outputDescr, _, outputCode) <- reverse types
+  | (inputType,  inputDescr,  inputCode, _)  <- types True
+  , (outputType, outputDescr, _, outputCode) <- types False
   ] ++
   [ ( "a", "error", evalErr ) ]
 
